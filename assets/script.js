39ae0540b6e9a56e61514cf2eee5a7099bac2c83
Samsonov-G <Samsonov-G>
2025-11-27 17:38:58 +0500

    created functions and changed type of input

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,39 @@
-const get_ready = document.querySelector('#get__ready'),
-    button_go = document.querySelector('.timer__button')
+document.addEventListener('DOMContentLoaded', function () {
+    const get_ready = document.querySelector('#get__ready'),
+        button_go = document.querySelector('.timer__button')
+
+    function getTime(field) {
+        const lastTwoChars = +field.value.slice(-2)
+
+        return lastTwoChars
+    }
+
+    function setTime(digits) {
+
+        get_ready.value = `00:0${digits}`
+
+        console.log('Time is over')
+
+    }
+
+    function timer(time) {
+        const intervalID = setInterval(() => {
+
+            time--
+
+            if (time === 0) {
+                clearInterval(intervalID)
+            }
+
+            setTime(time)
+        }, 1000)
+    }
 
 
-function timer(time) {
-    const intervalID = setInterval(() => {
-        console.log(`Count: ${time--}`)
-        if (time === 0) {
-            clearInterval(intervalID)
+    button_go.addEventListener('click', function (event) {
+        event.preventDefault()
 
-            console.log('Time is over')
-        }
-    }, 1000)
-}
+        timer(getTime(get_ready))
 
-
-button_go.addEventListener('click', function (event) {
-    event.preventDefault()
-
-    let get_ready_value = +get_ready.value
-
-    timer(get_ready_value)
-
+    })
 })
